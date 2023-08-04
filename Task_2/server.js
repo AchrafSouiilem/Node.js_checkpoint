d203074bb3c25ede87ced13db6016d0a53899d82
AchrafSouiilem <Achraf.souiilem@gmail.com>
2023-08-04 19:11:56 +0100

    update Task_2

--- Task_2/server.js
+++ Task_2/server.js
@@ -1,15 +1,10 @@
 var http = require('http');
-var fs = require('fs');
+
 
 http.createServer(function(req, res){
-    fs.readFile('index.html',function (err, data){
-        if(err) {
-            console.error(err);
-            return;
-        }
-        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
-        res.write(data);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write("<h1>Hello Node!!!!</h1>\n");
         res.end();
+    }).listen(3000,() =>{
+        console.log('Server running at http://127.0.0.1:3000');
     });
-}).listen(3000);
-console.log('Server running at http://127.0.0.1:3000');
